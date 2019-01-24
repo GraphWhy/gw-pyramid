c09161a85b2a33a8600388eb9562571ccc74bd2b
AlexanderMcNulty <alexander.mcnulty92@gmail.com>
2019-01-23 23:32:21 -0500

    refactor (general) vote_record

--- graphwhy/models/vote_record.py
+++ graphwhy/models/vote_record.py
@@ -1,7 +1,7 @@
 import datetime #<- will be used to set default dates on models
-from graphwhy.models.meta import Base  #<- we need to import our sqlalchemy metadata from which model classes will inherit
-from graphwhy.models.user import User  #<- we need to import our sqlalchemy metadata from which model classes will inherit
-from graphwhy.models.question_record import QuestionRecord  #<- we need to import our sqlalchemy metadata from which model classes will inherit
+from .meta import Base  #<- we need to import our sqlalchemy metadata from which model classes will inherit
+#from graphwhy.models.user import User  #<- we need to import our sqlalchemy metadata from which model classes will inherit
+#from graphwhy.models.question_record import QuestionRecord  #<- we need to import our sqlalchemy metadata from which model classes will inherit
 from sqlalchemy import (
     Column,
     Integer,
@@ -10,18 +10,23 @@ from sqlalchemy import (
     DateTime,    #<- time abstraction field
     ForeignKey,
 )
+from sqlalchemy.orm import relationship
 
 from webhelpers2.date import distance_of_time_in_words #<- human friendly dates
 
 class VoteRecord(Base):
     __tablename__ = 'votes'
     id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey(User.id))
-    question_id = Column(Integer, ForeignKey(QuestionRecord.id))
     vote = Column(Integer, nullable=False)
     created = Column(DateTime, default=datetime.datetime.utcnow)
     edited = Column(DateTime, default=datetime.datetime.utcnow)
     __table_args__ = {'extend_existing': True}
+    
+    question_id = Column(ForeignKey('questions.id'), nullable=False)
+    question = relationship('QuestionRecord', backref='general_question_votes')
+
+    creator_id = Column(ForeignKey('users.id'), nullable=False)
+    creator = relationship('User', backref='user_general_votes') 
 
     @property
     def created_in_words(self):
--- graphwhy/services/question_templating.py
+++ graphwhy/services/question_templating.py
@@ -8,11 +8,13 @@ class QuestionTemplateService(object):
     def template_prep(cls, request):
         page = int(request.params.get('page', 1))
         paginator = QuestionRecordService.get_paginator(request, page)
+
         author_list = UserService.all_users(request)
         paginator.totalUserVotes = 0
         # total the votes on each question
         #there must be a better way to do this
         query = request.dbsession.query(VoteRecord)
+       
         # time enfficieny is n^2 
         for question in paginator.items:
             question.author = author_list[question.user_id - 1].name
@@ -20,9 +22,11 @@ class QuestionTemplateService(object):
             for vote in query:
                 if vote.question_id == question.id:
                     question.totalVotes += vote.vote
+       
+
         #calculate the totalUserVotes
         for vote in query:
-            if author_list[vote.user_id-1].name == request.authenticated_userid:
+            if author_list[vote.creator_id-1].name == request.authenticated_userid:
                 paginator.totalUserVotes += 1
         return paginator
         
